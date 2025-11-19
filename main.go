442dd679dc0d42e6d72f612ece05254b22b61de0
ardentseeker <krdivakar65@gmail.com>
2025-11-19 18:20:43 +0530

    this push has for loop except range usage

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"slices"
+	"log"
 )
 
 func main() {
@@ -79,18 +78,41 @@ func main() {
 		arr2[3] = 100
 		fmt.Println("Modified Array:", arr2)
 	*/
-	var s = []int{10, 20, 30, 40, 50}
-	fmt.Println(reflect.TypeOf(s))
-	s = append(s, 60, 70, 80)
-	fmt.Println(s)
-	s = slices.DeleteFunc(s, func(v int) bool {
-		return v == 20 || v == 40 || v == 60
-	})
-	fmt.Println(s)
-	s = append(s, 90, 100)
-	fmt.Println(s)
+	/*
+		// use of Slice and its inbuilt functions
+		var s = []int{10, 20, 30, 40, 50}
+		fmt.Println(reflect.TypeOf(s))
+		s = append(s, 60, 70, 80)
+		fmt.Println(s)
+		s = slices.DeleteFunc(s, func(v int) bool {
+			return v == 20 || v == 40 || v == 60
+		})
+		fmt.Println(s)
+		s = append(s, 90, 100)
+		fmt.Println(s)
 
-	s = slices.Delete(s, 1, 4)
-	fmt.Println(s)
+		s = slices.Delete(s, 1, 4)
+		fmt.Println(s)
+	*/
+
+	//experiment with for loop
+	for i := 0; i < 10; i++ {
+		fmt.Print(i, " ")
+	}
+	log.Println("this that")
+
+	i := 0
+	for i < 10 {
+		fmt.Print(i, " ")
+		i++
+	}
+	log.Println("this that")
+	for {
+		if i <= 5 {
+			break
+		}
+		fmt.Print(i, " ")
+		i--
+	}
 
 }
